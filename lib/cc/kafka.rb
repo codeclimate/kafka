999f6b88a054f23346cee737ace0a3a907f647e4
patrick brisbin <pbrisbin@gmail.com>
2015-06-25 18:56:46 -0400

    Implement consumers

--- lib/cc/kafka.rb
+++ lib/cc/kafka.rb
@@ -1,2 +1,10 @@
 require "cc/kafka/consumer"
 require "cc/kafka/producer"
+
+module CC
+  module Kafka
+    class << self
+      attr_accessor :db, :offset_model, :logger
+    end
+  end
+end
---
+++ lib/cc/kafka/consumer.rb
@@ -0,0 +1,61 @@
+require "poseidon"
+
+module CC
+  module Kafka
+    class Consumer
+      def initialize(client_id, seed_brokers, topic, partition)
+        @offset = Kafka.offset_model.find_or_create!(
+          topic: topic,
+          partition: partition,
+        )
+
+        Kafka.logger.debug("offset: #{@offset.topic}/#{@offset.partition} #{@offset.current}")
+
+        @consumer = Poseidon::PartitionConsumer.consumer_for_partition(
+          client_id,
+          seed_brokers,
+          @offset.topic,
+          @offset.partition,
+          @offset.current
+        )
+      end
+
+      def on_message(&block)
+        @on_message = block
+      end
+
+      def start
+        trap(:TERM) { stop }
+
+        @running = true
+
+        while @running do
+          fetch_messages
+        end
+
+        Kafka.logger.info("shutting down due to TERM signal")
+        @consumer.close
+      end
+
+      def stop
+        @running = false
+      end
+
+      private
+
+      def fetch_messages
+        @consumer.fetch.each do |message|
+          @offset.set(current: message.offset + 1)
+
+          Kafka.db.transaction do
+            @on_message.call(BSON.deserialize(message.value))
+          end
+        end
+      rescue Poseidon::Errors::UnknownTopicOrPartition
+        Kafka.logger.debug("topic #{@topic} not created yet")
+
+        sleep 1
+      end
+    end
+  end
+end
