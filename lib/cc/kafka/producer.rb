f19bae7173d8fae59b86e92f38893d3a04bd01e2
patrick brisbin <pbrisbin@gmail.com>
2015-06-30 11:59:30 -0400

    Support http:// URLs in Producer
    If given an http:// URL, expect to be talking to
    
      https://github.com/stealthly/dropwizard-kafka-http
    
    which supports POSTs to /message with topic, message, and key
    parameters.

--- lib/cc/kafka/producer.rb
+++ lib/cc/kafka/producer.rb
@@ -4,6 +4,13 @@ require "poseidon"
 module CC
   module Kafka
     class Producer
+      SCHEMES = [
+        HTTP = "http",
+        KAFKA = "kafka",
+      ]
+
+      HTTPError = Class.new(StandardError)
+
       def initialize(client_id, url)
         @client_id = client_id
         @url = url
@@ -11,20 +18,51 @@ module CC
 
       def send_message(data, key = nil)
         serialized = BSON.serialize(data).to_s
-        message = Poseidon::MessageToSend.new(topic, serialized, key)
 
-        producer.send_messages([message])
-      rescue
-        close
-
-        raise
+        if http?
+          send_http(serialized, key)
+        else
+          send_poseidon(serialized, key)
+        end
       end
 
       def close
-        producer.close
+        unless http?
+          producer.close
+        end
       end
 
       private
+
+      def http?
+        uri.scheme == HTTP
+      end
+
+      def send_http(serialized, key)
+        data = {
+          "topic" => topic,
+          "message" => serialized,
+        }
+        data["key"] = key if key
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Post.new("/message")
+        request.set_form_data(data)
+
+        response = http.request(request)
+
+        unless response.is_a?(Net::HTTPSuccess)
+          raise HTTPError, "request not successful: #{response.inspect}"
+        end
+      end
+
+      def send_poseidon(serialized, key)
+        message = Poseidon::MessageToSend.new(topic, serialized, key)
+        producer.send_messages([message])
+      rescue
+        producer.close
+        raise
+      end
 
       def producer
         @producer ||= Poseidon::Producer.new(
