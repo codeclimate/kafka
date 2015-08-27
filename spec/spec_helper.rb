b8edfcc1492e33cb7dcf76d18d3cd7f045922708
Will Fleming <will@mintdigital.com>
2015-08-27 18:10:43 -0400

    style: don't setup mongo connection globally

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,12 +7,12 @@ Dotenv.overload(".env", ".env.test", ".env.test.local")
 
 CC::Kafka.logger.level = Logger::ERROR
 
-$mongo = Mongo::MongoClient.from_uri(ENV['KAFKA_MONGODB_URL'])
-
-Minidoc.connection = $mongo
-Minidoc.database_name = $mongo.db.name
-
 RSpec.configure do |conf|
+  mongo = Mongo::MongoClient.from_uri(ENV['KAFKA_MONGODB_URL'])
+
+  Minidoc.connection = mongo
+  Minidoc.database_name = mongo.db.name
+
   conf.before do
     Minidoc::TestHelpers.clear_database
   end
