b99f0bb3058cc5d3b87dd240d8d24e69a1176e94
patrick brisbin <pbrisbin@gmail.com>
2017-01-05 17:48:36 -0500

    Don't use Minidoc::Indexes
    Ensuring indexes at runtime is considered dangerous and such functionality was
    removed from the newest minidoc. Having this here forces any users of this gem
    to stay on older minidoc.

--- lib/cc/kafka/offset_storage/minidoc.rb
+++ lib/cc/kafka/offset_storage/minidoc.rb
@@ -4,13 +4,9 @@ module CC
       module Minidoc
         def self.included(base)
           base.extend ClassMethods
-          base.include ::Minidoc::Indexes
-
           base.attribute :topic, String
           base.attribute :partition, Integer
           base.attribute :current, Integer
-
-          base.ensure_index [:partition, :topic], unique: true
         end
 
         module ClassMethods
